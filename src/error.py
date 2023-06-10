bf22ee495f884cf48d5c78440ef3fed1abbc63a6
Violetta <lotsmanovavioletta@gmail.com>
2023-06-10 23:29:39 +0300

    changed error.py

--- src/error.py
+++ src/error.py
@@ -1,10 +1,3 @@
-class AddError(Exception):
-    """
-    Класс исключения добавления в файл
-    """
-    def __init__(self, text='Вакансия уже есть в файле'):
-        super().__init__(text)
-
 
 class DeleteError(Exception):
     """
