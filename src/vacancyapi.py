fc1465694435f23b47424ed0109bca13b088d17c
Violetta <lotsmanovavioletta@gmail.com>
2023-07-16 13:47:13 +0300

    fixed filesaver.py, func_user.py, vacancy.py, vacancyapi.py

--- src/vacancyapi.py
+++ src/vacancyapi.py
@@ -20,15 +20,16 @@ class HeadHunterAPI(VacancyAPI):
     Класс для работы с API hh.ru
     """
 
-    def __init__(self, url='https://api.hh.ru/vacancies'):
+    def __init__(self, url='https://api.hh.ru/vacancies') -> None:
         """
         Инициализация объекта класса
         """
+
         self.__base_url = url
 
 
     @property
-    def url(self):
+    def url(self) -> str:
         return self.__base_url
 
 
@@ -55,30 +56,32 @@ class SuperJobAPI(VacancyAPI):
     Класс для работы с API superjob.ru
     """
 
-    def __init__(self, url='https://api.superjob.ru/2.0/vacancies'):
+    def __init__(self, url='https://api.superjob.ru/2.0/vacancies') -> None:
         """
         Инициализация объекта класса
         """
-        self.api_key =  os.environ.get('API_KEY_SUPERJOB')
+
+        self.api_key = os.environ.get('API_KEY_SUPERJOB')
         self.__base_url = url
         self.__headers = {'X-Api-App-Id': self.api_key}
 
 
     @property
-    def url(self):
+    def url(self) -> str:
         return self.__base_url
 
 
     @property
-    def headers(self):
+    def headers(self) -> dict:
         return self.__headers
 
 
-    def get_vacancies(self, keyword) -> None:
+    def get_vacancies(self, keyword: str) -> None:
         """
         Получение информации о вакансиях через API
         :return: None
         """
+
         # делаем запрос к API
         url = f'{self.__base_url}?keyword={keyword}'
         response = requests.get(url, headers=self.__headers)
@@ -87,4 +90,3 @@ class SuperJobAPI(VacancyAPI):
             return vacancies
         else:
             print(f'Ошибка при выполнении запроса: {response.status_code}')
-
